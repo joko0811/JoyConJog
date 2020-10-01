17628f8909d9507eca33bcd9bb4dfbd566d9e842
joko <53999148+joko0811@users.noreply.github.com>
2020-10-01 18:27:31 +0900

    [merge]newFeatureの手動マージ漏れマージ　まーじか

--- Assets/Scripts/DestroyTarget.cs
+++ Assets/Scripts/DestroyTarget.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class DestroyTarget : MonoBehaviour
 {
+    GameObject player;
+
+    public float destroyRange = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        player = GameObject.FindGameObjectsWithTag("Player")[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (this.transform.position.z < player.transform.position.z - destroyRange)
+        {
+            Destroy(this.gameObject);
+        }
     }
     void OnCollisionEnter(Collision other)
     {

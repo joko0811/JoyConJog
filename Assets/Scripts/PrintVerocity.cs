d18ad52a679610ad1c15a6cd2503975c2271bdb6
otyamura <suzuki08suzuki@gmail.com>
2020-11-03 14:54:16 +0900

    add AddForce

---
+++ Assets/Scripts/PrintVerocity.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrintVerocity : MonoBehaviour
+{
+    public GameObject score_object = null; // Textオブジェクト
+    public int score_num = 0; // スコア変数
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // オブジェクトからTextコンポーネントを取得
+        Text score_text = score_object.GetComponent<Text>();
+        // テキストの表示を入れ替える
+        score_text.text = "Score:" + score_num;
+
+        score_num += 1; // とりあえず1加算し続けてみる
+
+    }
+}
+
--- Assets/Scripts/VRController.cs
+++ Assets/Scripts/VRController.cs
@@ -5,34 +5,45 @@ using UnityEngine;
 public class VRController : MonoBehaviour
 {
     //Rigidbodyを変数に入れる
+    Rigidbody rb;
     //移動スピード
-    public float speed = 3f;
-    //ジャンプ力
-    public float thrust = 200;
+    float speed = 3f;
+    //スピードに対しての定数倍
+    public float mulSpeed = 1.5f;
     //ユニティちゃんの位置を入れる
     Vector3 playerPos;
     //地面に接触しているか否か
     bool ground;
     public bool isJoycon;
+    float velocity = 0;
+    float tmp = 0;
+    Vector3 firstPos;
 
     //joycon関係の変数
     private List<Joycon> m_joycons;
     private Joycon m_joyconL;
     private Joycon m_joyconR;
+    private GUIStyle style;
 
     // Start is called before the first frame update
     void Start()
     {
         //Rigidbodyを取得
+        rb = GetComponent<Rigidbody>();
 
         //ユニティちゃんの現在より少し前の位置を保存
         playerPos = transform.position;
+        firstPos = transform.position;
+
 
         //joycon準備
         m_joycons = JoyconManager.Instance.j;
         if (m_joycons == null || m_joycons.Count <= 0) return;
         m_joyconL = m_joycons.Find(c => c.isLeft);
         m_joyconR = m_joycons.Find(c => !c.isLeft);
+        style = new GUIStyle();
+        style.fontSize = 60;
+        
     }
 
     // Update is called once per frame
@@ -44,44 +55,53 @@ public class VRController : MonoBehaviour
             //var accelR = m_joyconR.GetAccel();  // 加速度
             //Debug.Log("accelR" + accelR);
             var accelL = m_joyconL.GetAccel();  // 加速度
-            Debug.Log("accelL" + accelL);
+            //Debug.Log("accelL" + accelL);
             //var accelAve = (accelR.magnitude + accelL.magnitude ) / 2.0f;
             float accelMag = accelL.magnitude;
-            const float mulSpeed = 1.5f;
             speed = accelMag * mulSpeed;
             accelMag /= 3.0f;
-            Debug.Log("accelMag" + accelMag);
+            //Debug.Log("accelMag" + accelMag);
+            Debug.Log("velocity" + ((transform.position - playerPos) / Time.deltaTime).magnitude);
 
-                //Debug.Log(accelMag);
-                if (accelMag > 0.4)
+
+
+            float z;
+            //Debug.Log(accelMag);
+            if (accelMag > 0.4)
+            {
+                //float x = Time.deltaTime * speed;
+                z = Time.deltaTime * speed;
+                //rb.MovePosition(transform.position + new Vector3(0, 0, z));
+                rb.AddForce(new Vector3(0, 0, z));
+                Vector3 direction = transform.position - playerPos;
+
+                //Debug.Log(direction.magnitude);
+                if (direction.magnitude > 0.01f)
                 {
-                    //float x = Time.deltaTime * speed;
-                    float z = Time.deltaTime * speed;
-                    Vector3 direction = transform.position - playerPos;
 
-                    Debug.Log(direction.magnitude);
-                    if (direction.magnitude > 0.01f)
-                    {
-
-                        transform.rotation = Quaternion.LookRotation(new Vector3
-                            (0, 0, direction.z));
-                    }
-                    //ユニティちゃんの位置を更新する
-                    transform.position += new Vector3(0, 0, z);
-
+                    transform.rotation = Quaternion.LookRotation(new Vector3
+                        (0, 0, direction.z));
+                }
+                //ユニティちゃんの位置を更新する
+                playerPos = transform.position;
+            }
+            else
+            {
+                //Stop();
             }
 
         }
         else
         {
 
-                //A・Dキー、←→キーで横移動
-                float x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
+            //A・Dキー、←→キーで横移動
+            float x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
 
                 //W・Sキー、↑↓キーで前後移動
                 float z = Input.GetAxisRaw("Vertical") * Time.deltaTime * speed;
 
                 //現在の位置＋入力した数値の場所に移動する
+                rb.MovePosition(transform.position + new Vector3(x, 0, z));
 
                 //ユニティちゃんの最新の位置から少し前の位置を引いて方向を割り出す
                 Vector3 direction = transform.position - playerPos;
@@ -96,13 +116,21 @@ public class VRController : MonoBehaviour
 
 
                 //ユニティちゃんの位置を更新する
-                transform.position += new Vector3(x, 0, z);
-
-
+                playerPos = transform.position;
         }
 
 
     }
+
+    private void FixedUpdate()
+    {
+        
+        //velocity = ((transform.position - firstPos) / Time.time).magnitude;
+        velocity = rb.velocity.magnitude;
+    }
+
+
+
     //Planに触れている間作動
     void OnCollisionStay(Collision col)
     {
@@ -121,5 +149,17 @@ public class VRController : MonoBehaviour
             m_joyconL.SetRumble(160, 320, 0.6f, 200);
 
         }
+    }
+
+    //止めたい時に呼び出し
+    void Stop()
+    {
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width - 300, Screen.height - 80, 500, 500), float.Parse(velocity.ToString("f2")) +"km/h",style);
     }
 }
